974a7342d49334fdb0e4c13df9ee262e853f2efa
Ervin Jahic <ervinjahic65@gmail.com>
2022-07-05 14:58:58 +0200

    added file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    FavoritesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
